e9e2aa29e019f0a1a3053dc911565b49de014fbf
Gabor Liptak <liptakgabor@freemail.hu>
2017-12-22 21:06:40 +0100

    Test cases are simplified

--- libdevice_test.cpp
+++ libdevice_test.cpp
@@ -1,3 +1,6 @@
+#include <assert.h>
+#include <math.h>
+
 #include "libdevice.h"
 #include "libdevice_adc.h"
 #include "libdevice_dio.h"
@@ -9,26 +12,22 @@ int main(int argc, const char **argv)
   registerAdcDevice();
   registerDioDevice();
   t_device_fd fd1 = device_open("/dev/adc/Ub", 0);
-  double Ub = 3.33, Ub2;
-  (void)device_write(fd1, &Ub, sizeof(Ub));
-  (void)device_read(fd1, &Ub2, sizeof(Ub2));
+  (void)device_adc_write_double(fd1, 3.33);
+  assert(fabs(device_adc_read_double(fd1)-3.33) < 1e-9);
   t_device_fd fd11 = device_open("/dev/adc/Uz", 0);
-  double Uz = 13;
-  (void)device_write(fd11, &Uz, sizeof(Uz));
+  (void)device_adc_write_double(fd11, 13.0);
   t_device_fd fd2 = device_open("/dev/adc/Ub", 0);
-  (void)device_read(fd2, &Ub2, sizeof(Ub2));
-  (void)device_read(fd11, &Uz, sizeof(Uz));
+  assert(fabs(device_adc_read_double(fd2)-3.33) < 1e-9);
+  assert(fabs(device_adc_read_double(fd11)-13.0) < 1e-9);
 
   t_device_fd fd3 = device_open("/dev/dio/Ub", 0);
-  char Ub3 = 1, Ub4;
-  (void)device_write(fd3, &Ub3, sizeof(Ub3));
-  (void)device_read(fd3, &Ub4, sizeof(Ub4));
+  (void)device_dio_write_char(fd3, 1);
+  assert(device_dio_read_char(fd3) == 1);
   t_device_fd fd4 = device_open("/dev/dio/Ub", 0);
   t_device_fd fd_pcv = device_open("/dev/dio/pcv", 0);
-  char pcv = 0;
-  (void)device_write(fd_pcv, &pcv, sizeof(pcv));
-  (void)device_read(fd4, &Ub4, sizeof(Ub4));
-  (void)device_read(fd_pcv, &pcv, sizeof(pcv));
+  (void)device_dio_write_char(fd_pcv, 0);
+  assert(device_dio_read_char(fd4) == 1);
+  assert(device_dio_read_char(fd_pcv) == 0);
   device_close(fd1);
   device_close(fd11);
   device_close(fd2);
